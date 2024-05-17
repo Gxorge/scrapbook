cc8e136cf464cf09d3cf54abf3245eb2c85268d0
George Hotten <github@ghott.me>
2024-05-17 01:53:56 +0100

    setup changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Scrapbook</title>
         @vite(['resources/scss/gweb.scss'])
     </head>
     <body class="gweb-site">
